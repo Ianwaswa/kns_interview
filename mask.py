90b65da61fe865df167418066d2ee18235f01449
Ianwaswa <ianwaswa2019@gmail.com>
2024-03-11 03:37:11 -0700

    push changes

---
+++ mask.py
@@ -0,0 +1,12 @@
+input_item : str
+        first_name: "Greg",
+        last_name: "Salama",
+        date_of_birth: "1990-06-01",
+        email: "salamander90@gmail.com"
+for n >= first_name:
+    if n == "Greg":
+        print("Gr**")
+    elif last_name == "Salama":
+        print("Sa****")
+    elif date_of_birth == "1990-06-01":
+        print("19**-0*-0*")
